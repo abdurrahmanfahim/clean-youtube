0e2ee6ce7a7942ca21ceb1bd5f9bb5ba20369b65
Abdur Rahman Fahim <127137394+abdurrahmanfahim@users.noreply.github.com>
2026-01-06 15:03:57 +0600

    Refactor PlayerPage and NotFound to separate files
    Moved PlayerPage and NotFound components from App.jsx to their own files under src/pages. Updated imports in App.jsx accordingly for better code organization and maintainability.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Navbar from "./components/navbar";
 import usePlaylist from "./hooks/usePlaylist";
 import PlaylistCardItem from "./components/playlist-card-item";
 import { Container, Grid, Typography } from "@mui/material";
+import PlayerPage from "./pages/player-page";
+import NotFound from "./pages/not-found";
 
 const HomePage = ({ playlistArray }) => {
   return (
@@ -22,31 +24,6 @@ const HomePage = ({ playlistArray }) => {
           ))}
         </Grid>
       )}
-    </Container>
-  );
-};
-
-const PlayerPage = ({ playlists }) => {
-  const { playlistId } = useParams();
-  const current = playlists[playlistId];
-  console.log("Current Course -->", current);
-
-  if (!current) {
-    return;
-  }
-
-  return (
-    <Container maxWidth={"lg"} sx={{ my: 16 }}>
-      <Typography variant="h2">{current.playlistTitle}</Typography>
-      <Typography variant="body1">{current.playlistDescription}</Typography>
-    </Container>
-  );
-};
-
-const NotFound = () => {
-  return (
-    <Container maxWidth={"lg"} sx={{ my: 16 }}>
-      <Typography variant="h2">404 | Page not found</Typography>
     </Container>
   );
 };
---
+++ src/pages/not-found/index.jsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <Container maxWidth={"lg"} sx={{ my: 16 }}>
+      <Typography variant="h2">404 | Page not found</Typography>
+    </Container>
+  );
+};
+
+export default NotFound;
